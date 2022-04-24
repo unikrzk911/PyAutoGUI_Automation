db1fb7b82b2370cfbe64c75b063e4d973db0fea2
Unique Rajak <unikrzk911@gmail.com>
2022-04-24 09:56:12 +0545

    Add files via upload

--- automatic_tiktok_liker.py
+++ automatic_tiktok_liker.py
@@ -9,18 +9,39 @@ def liker(num):
         time.sleep(1)
         pyautogui.doubleClick()
         time.sleep(1)
-        pyautogui.moveTo(782, 418)
+        pyautogui.press('down')
         time.sleep(1)
-        pyautogui.leftClick()
 
 
-print("Open the account whose video you want to like in web browser with the video open")
+def unliker(num):
+    time.sleep(5)
+    for i in range(num):
+        liked_cord = pyautogui.locateCenterOnScreen('liked.png')
+        pyautogui.leftClick(liked_cord)
+        time.sleep(1)
+        pyautogui.press('down')
+        time.sleep(1)
+
+
+print("Open the account whose video you want to like in web browser with the video open\n")
 while True:
-    confirmation = input('\nOpened yet?(Y/N):')
-    if confirmation == 'y' or confirmation == 'Y':
-        videos_to_like = int(input("How many videos you want to like?:"))
-        liker(videos_to_like)
-        break
+    print('What do you want to do?')
+    print('1. Like videos')
+    print('2. Unlike videos')
+    print('3. Exit')
+    choice = int(input('Enter your choice:'))
+    if choice == 1:
+        n = int(input('How many videos you want to like?'))
+        liker(n)
+        exit()
+    elif choice == 2:
+        n = int(input('How many videos you want to unlike?'))
+        unliker(n)
+        exit()
+    elif choice == 3:
+        exit()
     else:
-        print('\nNote: Open the profile in the browser')
+        print('\nInvalid choice.')
         continue
+
+
